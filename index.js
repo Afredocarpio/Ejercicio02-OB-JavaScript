aeb6924a4e1c0977b9feae74d87264f64c662711
Alfredo Carpio <85145624+Afredocarpio@users.noreply.github.com>
2022-10-04 12:33:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const listInfo = {
+    nombre: "Alfredo Carpio",
+    edad: 30,
+    isDev: true,
+    "fecha-nacimiento": new Date(1992, 7, 21),
+    "libro-favoritos": {
+        titulo: "El caballero de la armadura oxidada" ,
+        autor: "Robert Fisher",
+        fecha: new Date("july 27 2005"),
+        url: new URL("https://www.amazon.com/Caballero-Armadura-Oxidada-Knight-Spanish/dp/849777230X")
+    }
+};
+
+console.log(listInfo);
